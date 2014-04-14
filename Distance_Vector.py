953adfea86606376ddd27b9d13d23fb36dbb8639
Joel Scott <toasteroverheated@gmail.com>
2014-04-14 04:35:22 -0400

    Done 2
    Little wonky, but it'll do for now.

--- Distance_Vector.py
+++ Distance_Vector.py
@@ -27,8 +27,6 @@ for line in myFile:
 vector = [[0 for x in range(3)] for x in range(lineCount)]
 newList = []
 toRemove = []
-x = len(vector)
-y = x + 1
 
 for i in range(lineCount):
 # interates through the number rows
@@ -37,23 +35,26 @@ for i in range(lineCount):
     # double loop allows us to initialize the double array
         vector[i][j] = fileArray[j + i * 3]
 
-    
 def main():
-    global vector, x, y
+    global vector
     
-#    print (vector)
     for i in range(len(vector)):
+    # adds a node to the node array
         if vector[i][0] not in nodeArray:
             nodeArray.append(vector[i][0])
         if vector[i][1] not in nodeArray:
             nodeArray.append(vector[i][1])
 
     for i in range(len(nodeArray)):
+    # adds the current node to current node distance to the vector
+    # i.e. A to A with distance 0
         vector.append([nodeArray[i], nodeArray[i], 0])
 
-
     for i in range(len(vector)):
+    # iterates through our list of nodes to compare
         for j in range(len(vector)):
+        # iterates through our list of nodes looking
+        # to compare neighbors
             if vector[j][1] == vector[i][0] and vector[j][0] != vector[i][1]:
                 decDist = (Decimal(vector[i][2]) + Decimal(vector[j][2]))
                 decDist = round(decDist, 1) # round to one decimal place
@@ -64,14 +65,27 @@ def main():
     vector.sort()
 
     newList = []
+    for i in range(len(nodeArray)):
+    # traverse through the list of nodes
+        for j in range(len(nodeArray)):
+        # traverse through the list of nodes and add them as distance 1000
+        # this is so duplicates node distances will be removed and the
+        # left over node paths of 1000 can be changed to N/R
+            vector.append([nodeArray[i], nodeArray[j], 1000])
+
+    vector.sort()
     for i in vector:
+    # iterates through our list looking for
+    # matches to remove
         if i not in newList:
             newList.append(i)
-            
-    vector = newList
 
+    vector = newList
     for i in range(len(vector)):
+    # iterates through the list of nodes to compare
         for j in range(len(vector)):
+        # iterates through the list of nodes
+        # to find and compare shortest path
             if i != j and (vector[i][0] == vector[j][0] and vector[i][1] == vector[j][1]):
                 if vector[i][2] > vector[j][2]:
                     toRemove.append([vector[i][0], vector[i][1], vector[i][2]])
@@ -79,21 +93,24 @@ def main():
                     toRemove.append([vector[j][0], vector[j][1], vector[j][2]])
 
     for i in range(len(toRemove)):
+    # iterates through the number of expensive traversals
+    # and removes them
         if toRemove[i] in vector:
-            vector.remove(toRemove[i])
+            vector.remove(toRemove[i]) 
+
+    for i in range(len(vector)):
+    # iterate through the list of nodes
+    # changing the not reachable nodes to N/R
+        if vector[i][2] == 1000:
+            vector[i][2] = 'N/R'
 
     for i in range(len(nodeArray)):
-        print ("Node: " + nodeArray[i])
-        print ("Distance from " + str(vector[6 * i][0]) + " to " + str(vector[6 * i][1]) + " is " + str(vector[6 * i][2]))
-        print ("Distance from " + str(vector[6 * i + 1][0]) + " to " + str(vector[6 * i + 1][1]) + " is " + str(vector[6 * i + 1][2]))
-        print ("Distance from " + str(vector[6 * i + 2][0]) + " to " + str(vector[6 * i + 2][1]) + " is " + str(vector[6 * i + 2][2]))
-        print ("Distance from " + str(vector[6 * i + 3][0]) + " to " + str(vector[6 * i + 3][1]) + " is " + str(vector[6 * i + 3][2]))
-        print ("Distance from " + str(vector[6 * i + 4][0]) + " to " + str(vector[6 * i + 4][1]) + " is " + str(vector[6 * i + 4][2]))
-        print ("Distance from " + str(vector[6 * i + 5][0]) + " to " + str(vector[6 * i + 5][1]) + " is " + str(vector[6 * i + 5][2]))
+    # iterates through our node list and prints the node
         print (' ')
-
-
-    
+        print ("Node: " + nodeArray[i])
+        for j in range(len(nodeArray)):
+        # double loops so we can print the correct nodes and values
+            print ("Distance from " + str(vector[j + i * len(nodeArray)][0]) + " to " + str(vector[j + i * len(nodeArray)][1]) + " is " + str(vector[j + i * len(nodeArray)][2]))
     
 if __name__ == '__main__':
     main()
